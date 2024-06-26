4b0f17ff4387008c6358826ca024595e4bac3db0
LABS\placement <sblock>
2024-06-26 11:35:25 +0530

    first commit

---
+++ backend/controller/ordercontroller.js
@@ -0,0 +1,17 @@
+// exports.createorder=(res,req,next)=>{
+//     res.josn(
+//         {
+//             success:true, 
+//             message:" order work!"
+//         }
+//     )
+// }
+
+exports.createorder=(req,res,next)=>{
+    res.json(
+        {
+            success:true, 
+            message:" order work!"
+        }
+    )
+}
---
+++ backend/controller/productcontroller.js
@@ -0,0 +1,12 @@
+exports.getproducts=(req,res,next)=>{
+    res.json({
+        success:true,
+        message:'get products working!'
+    })
+}
+exports.getsingleproducts=(req,res,next)=>{
+    res.json({
+        success:true,
+        message:'get single products working!'
+    })
+}
